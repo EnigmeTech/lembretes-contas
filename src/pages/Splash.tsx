d0c5f3323d9a72265ccbc6ebec66d95acee9dea8
Geanne1999 <gtfo.eng17@uea.edu.br>
2025-07-15 17:08:36 -0400

    add enigme tech logo

--- src/pages/Splash.tsx
+++ src/pages/Splash.tsx
@@ -15,23 +15,46 @@ export default function SplashScreen() {
   }, [navigate]);
 
   return (
-    <Box
-      height="100vh"
-      width="100vw"
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      flexDirection="column"
-      sx={{
-        background: "linear-gradient(135deg, #1976d2, #42a5f5)",
-        color: "white",
-      }}
-    >
-      <img src={Logo} alt="Logo" className="splash-logo" />
+    <>
+      <Box
+        height="100vh"
+        width="100vw"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+        sx={{
+          background: "linear-gradient(135deg, #1976d2, #42a5f5)",
+          color: "white",
+        }}
+      >
+        <img src={Logo} alt="Logo" className="splash-logo" />
 
-      <Typography variant="h4" fontWeight="bold">
-        NextDue
-      </Typography>
-    </Box>
+        <Typography variant="h4" fontWeight="bold">
+          NextDue
+        </Typography>
+        <footer
+          style={{
+            position: "absolute",
+            bottom: 20,
+            left: 0,
+            right: 0,
+            textAlign: "center",
+          }}
+        >
+          <Box display="flex" alignItems="center" justifyContent="center">
+            <Typography variant="caption" color="white" mr={1}>
+              Produced by
+            </Typography>
+            <img
+              src="/logoEnigme.png"
+              alt="Logo Enigme"
+              className="footer-logo"
+              style={{ height: 60 }}
+            />
+          </Box>
+        </footer>
+      </Box>
+    </>
   );
 }
